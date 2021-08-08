4f3c97d756ff5423af57c7e43a918bbd634f672c
Ermias Bizuwork <ebiz@ermiass-mbp.lan>
2021-08-08 16:28:13 -0400

    generally setup new cluster

---
+++ flask_app/src/backend_api/models/datatest.py
@@ -0,0 +1,39 @@
+"""Class definition for User model."""
+from datetime import datetime, timezone, timedelta
+from uuid import uuid4
+
+import jwt
+from flask import current_app
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from backend_api import db, bcrypt
+from backend_api.util.result import Result
+from backend_api.util.datetime_util import (
+    utc_now,
+    get_local_utcoffset,
+    make_tzaware,
+    localized_dt_string,
+)
+
+class Datatest(db.Model):
+    "test data model for storing whatever and such"
+    __tablename__ = "test_data"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    date = db.Column(db.DateTime, default=utc_now)
+    data = db.Column(db.JSON, )
+
+    def __repr__(self):
+        return (
+            f"<id={self.id}, name= {self.name}, date={self.date}, data={self.data}>"
+        )
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
